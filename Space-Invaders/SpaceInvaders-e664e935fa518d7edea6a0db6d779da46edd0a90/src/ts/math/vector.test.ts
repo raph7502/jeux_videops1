b03c378de8f09beb3a4aa69b901513faa0571e23
Raphael Bin <raphael.bin@epitech.eu>
2026-04-13 14:55:15 +0200

    All official unit tests are now passing

---
+++ Space-Invaders/SpaceInvaders-e664e935fa518d7edea6a0db6d779da46edd0a90/src/ts/math/vector.test.ts
@@ -0,0 +1,24 @@
+import { add, subtract, dot, normalize } from './vector';
+
+describe('Tests unitaires Vecteurs - Space Invaders', () => {
+    const v1 = { x: 5, y: 50 };
+    const v2 = { x: 10, y: 100 };
+
+    test('add devrait additionner deux vecteurs', () => {
+        expect(add(v1, v2)).toEqual({ x: 15, y: 150 });
+    });
+
+    test('subtract devrait soustraire deux vecteurs', () => {
+        expect(subtract(v1, v2)).toEqual({ x: -5, y: -50 });
+    });
+
+    test('dot devrait calculer le produit scalaire', () => {
+        expect(dot(v1, v2)).toBe(5050);
+    });
+
+    test('normalize devrait normaliser le vecteur', () => {
+        const result = normalize(v1);
+        expect(result.x).toBeCloseTo(0.09950371902099892, 10);
+        expect(result.y).toBeCloseTo(0.9950371902099892, 10);
+    });
+});                                                                                                                     
